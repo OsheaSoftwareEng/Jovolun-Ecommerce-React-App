b8721b44687a04735f0193248095c94c73836e8e
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-12-30 12:24:34 -0500

    updated index component for mainview

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import MainView from './Components/MainView/MainView';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, Container, SimpleGrid } from '@chakra-ui/react';
+import NavigationBar from './Components/NavigationBar/NavigationBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <ChakraProvider>
-    <MainView/>
+    <Container maxW='2x0'>
+      <NavigationBar />
+      <SimpleGrid minChildWidth='300px' spacing='40px'>
+        <MainView />
+      </SimpleGrid>
+    </Container>
   </ChakraProvider>
 );
-
-
