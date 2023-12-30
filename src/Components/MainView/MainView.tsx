8a82778da651fcc818333ca95dd545f6a1c434ac
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-12-30 11:29:49 -0500

    updated types in mainview

--- src/Components/MainView/MainView.tsx
+++ src/Components/MainView/MainView.tsx
@@ -6,28 +6,34 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ItemCard from '../ItemCard/ItemCard';
 import NavigationBar from '../NavigationBar/NavigationBar';
 import ItemView from '../ItemsView/ItemsView';
+import { Link } from 'react-router-dom';
 
 //butter initalization for databse
-const butter: any = Butter(process.env.REACT_APP_BUTTER_ECOMMERCE as string);
+const butter: any = Butter(process.env.REACT_APP_BUTTER_ECOMMERCE as any);
 
 function MainView() {
   //products from butter ecommerce database
-  const [products, setProducts] = useState<any[]>([]);
+  const [products, setProducts] = useState<string[] | number[]>([]);
 
   useEffect(() => {
     async function fetchData() {
       const res = await butter.content.retrieve(['jovos'], {
-        order: 'name'
+        order: 'id'
       });
       const { data } = await res.data;
+      console.log(data);
       const allProducts = data.jovos;
       setProducts(allProducts);
+      console.log(allProducts);
+     
     }
 
     fetchData();
   }, []);
 
-  return (
+ 
+
+  return ( 
     <>
       <Container marginLeft={10} maxW='container.xl'>
         <NavigationBar />
@@ -36,11 +42,19 @@ function MainView() {
         <Routes>
           <Route
             path='/'
-            element={products.map((products) => (
-              <div style={{ display: 'inline-block' }}>
-                <Container marginLeft={10} maxW='container.md'>
-                  <SimpleGrid minChildWidth='300px' spacing='40px'>
-                    <ItemCard products={products} />
+            element={products.map((products: any ) => (
+              <div style={{ display: 'inline-block' }} key={products.id}>
+                <Container
+                  marginLeft={10}
+                  maxW='container.md'
+                  key={products.id}
+                >
+                  <SimpleGrid
+                    minChildWidth='300px'
+                    spacing='40px'
+                    key={products.id}
+                  >
+                    <ItemCard products={products} key={products.id} />
                   </SimpleGrid>
                 </Container>
               </div>
@@ -48,6 +62,10 @@ function MainView() {
           />
           <Route
             path='/products/:id'
+            // Component={(products: any) => (
+            //   <ItemView products={products} findProduct={findProduct} />
+            // )}
+
             element={<ItemView products={products} />}
           />
         </Routes>
