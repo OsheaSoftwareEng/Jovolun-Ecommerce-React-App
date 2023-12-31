88cdb06473f690f434973cbeb02f5e0a8f679e7a
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-12-31 16:29:19 -0500

    updated mainview format layout added code to keep track of navigation

--- src/Components/MainView/MainView.tsx
+++ src/Components/MainView/MainView.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import Butter from 'buttercms';
-import { Container, SimpleGrid } from '@chakra-ui/react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  Container,
+  SimpleGrid,
+  Box,
+  AbsoluteCenter,
+  Divider
+} from '@chakra-ui/react';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate
+} from 'react-router-dom';
 import ItemCard from '../ItemCard/ItemCard';
 import NavigationBar from '../NavigationBar/NavigationBar';
 import ItemView from '../ItemsView/ItemsView';
-import { Link } from 'react-router-dom';
+import ItemFilter from '../ItemFilter/ItemFilter';
+import ViewItemLogo from '../ViewItemLogo/ViewItemLogo';
 
 //butter initalization for databse
 const butter: any = Butter(process.env.REACT_APP_BUTTER_ECOMMERCE as any);
@@ -14,6 +27,24 @@ const butter: any = Butter(process.env.REACT_APP_BUTTER_ECOMMERCE as any);
 function MainView() {
   //products from butter ecommerce database
   const [products, setProducts] = useState<any>([]);
+
+  const [showItemFilter, setShowItemFilter] = useState(true);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleItemFilterClick = () => {
+    setShowItemFilter(false);
+    navigate('/');
+  };
+  useEffect(() => {
+    // Reset state and show navigation bar when navigating to the main page
+    if (location.pathname === '/') {
+      setShowItemFilter(true);
+    } else {
+      setShowItemFilter(false);
+    }
+  }, [location.pathname]);
 
   useEffect(() => {
     async function fetchData() {
@@ -25,34 +56,35 @@ function MainView() {
       const allProducts = data.jovos;
       setProducts(allProducts);
       console.log(allProducts);
-     
     }
-
     fetchData();
   }, []);
 
- 
-
-  return ( 
-    <> 
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path='/'
-            element={products.map((products: any ) => (
-                    <ItemCard products={products} key={products.id} />
-            ))}
-          />
-          <Route
-            path='/products/:id'
-            // Component={() => (
-            //   <ItemView products={products}/>
-            // )}
-
-            element={<ItemView products={products} />}
-          />
-        </Routes>
-      </BrowserRouter>
+  return (
+    <>
+      <Container maxW='2x0'>
+        <NavigationBar />
+        {showItemFilter && <ItemFilter products={products} />}
+        <ViewItemLogo products={products} />
+        <SimpleGrid minChildWidth='300px' spacing='40px'>
+          <Routes>
+            <Route
+              path='/'
+              element={products.map((products: any) => (
+                <ItemCard
+                  products={products}
+                  key={products.id}
+                  onItemClick={handleItemFilterClick}
+                />
+              ))}
+            />
+            <Route
+              path='/products/:id'
+              element={<ItemView products={products} />}
+            />
+          </Routes>
+        </SimpleGrid>
+      </Container>
     </>
   );
 }
