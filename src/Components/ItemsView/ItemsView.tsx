434e52f2930fd5bb9db73ecc02276d50936d8861
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-12-31 16:28:22 -0500

    fixed bug on item view to view product details

--- src/Components/ItemsView/ItemsView.tsx
+++ src/Components/ItemsView/ItemsView.tsx
@@ -1,46 +1,48 @@
-import { Image } from '@chakra-ui/react';
+import { Image, SimpleGrid, AbsoluteCenter, Divider } from '@chakra-ui/react';
 import * as React from 'react';
 import { Box } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
+import { Spinner } from '@chakra-ui/react';
+import ViewItemLogo from '../ViewItemLogo/ViewItemLogo';
 
-const ItemView = ({ products }: { products: any}) => {
-
-  const  {id}= useParams<{id: string}>();
+const ItemView = ({ products }: { products: any }) => {
+  const { id } = useParams<{ id: any }>();
   console.log(id);
 
-  console.log(products);
-  console.log(products.find((product: any) => product.name === 'TOM FORD'));
+  const productID = parseInt(id);
 
-//   const productsDetail = products.filter(
-//     (product: any) => product.id === id
-//   );
-
-const findProduct = products.find((product: any) => product.id === id);
+  const findProduct = products.find((product: any) => product.id === productID);
 
   console.log(findProduct);
 
   return (
-    <Box
-      bg='white'
-      maxW='sm'
-      borderWidth='1px'
-      rounded='lg'
-      shadow='lg'
-      _hover={{ shadow: 'dark-lg' }}
-    >
-      <Image
-        h='350px'
-        fit='cover'
-        src={findProduct.image}
-        alt={`Picture of ${findProduct.name}`}
-        mt={0}
-        p={1}
-        borderRadius={15}
-        justifyContent='center'
-        ml={10}
-        marginTop={5}
-      />
-    </Box>
+    <>
+      <ViewItemLogo products={products} />
+      <SimpleGrid>
+        {findProduct ? (
+          <Image
+            h='350px'
+            fit='cover'
+            src={findProduct.image}
+            alt={`Picture of ${findProduct.name}`}
+            mt={0}
+            p={1}
+            borderRadius={15}
+            justifyContent='center'
+            ml={10}
+            marginTop={5}
+          />
+        ) : (
+          <Spinner
+            thickness='4px'
+            speed='0.55s'
+            emptyColor='gray.200'
+            color='green'
+            size='xl'
+          />
+        )}
+      </SimpleGrid>
+    </>
   );
 };
 
