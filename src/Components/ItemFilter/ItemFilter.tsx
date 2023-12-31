0ba45b7627340a562daa393cf640420c7d5d7c99
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-12-31 16:28:03 -0500

    made a component for itemfiltering

---
+++ src/Components/ItemFilter/ItemFilter.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton
+} from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
+import { useDisclosure } from '@chakra-ui/react';
+import './ItemFilter.css';
+import FilterListItem from '../FilterListItem/FilterListItem';
+
+const ItemFilter = ({ products }: { products: any }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef: any = React.useRef();
+
+  return (
+    <>
+      <div className='filterOptions'>
+        <Button
+          ref={btnRef}
+          variant='outline'
+          colorScheme='grey'
+          onClick={onOpen}
+        >
+          Brands
+        </Button>
+        <Button
+          ref={btnRef}
+          variant='outline'
+          colorScheme='grey'
+          onClick={onOpen}
+        >
+          Price
+        </Button>
+        <Button
+          ref={btnRef}
+          variant='outline'
+          colorScheme='grey'
+          onClick={onOpen}
+        >
+          Fragrance Size
+        </Button>
+        <Button
+          ref={btnRef}
+          variant='outline'
+          colorScheme='grey'
+          onClick={onOpen}
+        >
+          Rating
+        </Button>
+      </div>
+      <Drawer
+        isOpen={isOpen}
+        size='md'
+        placement='right'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Filters</DrawerHeader>
+
+          <DrawerBody>
+            <FilterListItem products={products} />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='blue'>Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default ItemFilter;
