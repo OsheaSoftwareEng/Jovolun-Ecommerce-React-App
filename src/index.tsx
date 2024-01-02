e4032d36e82744f8a8977d6a069eb6e59c02bc14
Oshea Sanders <osheasoftwareeng@gmail.com>
2024-01-01 21:05:27 -0500

    cleaned up unused imports

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import MainView from './Components/MainView/MainView';
-import { ChakraProvider, Container, SimpleGrid } from '@chakra-ui/react';
-import NavigationBar from './Components/NavigationBar/NavigationBar';
-import ItemFilter from './Components/ItemFilter/ItemFilter';
+import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
